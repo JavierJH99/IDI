8bf4d92a0dbd0e1245fd15f32e1a6cfbb52291a9
JavierJH99 <javierjimenezhonrado@gmail.com>
2023-02-15 11:33:53 +0100

    Arreglado

--- getCorrectAnswer.js
+++ getCorrectAnswer.js
@@ -8,23 +8,47 @@ preguntas = document.getElementsByClassName("qtext");
 opciones = document.getElementsByClassName("answer");
 json = "";
 
+//Si has accedido posteriormente a la revisión: 0
+//Si es la revisión que aparece al acabar el test: 1
+revision = 0;
+
 setTimeout(() => {
+    if (confirm('Primera revision')) revision = 1
+    else revision = 0
+
     for(let i = 0; i < preguntas.length; i++) {
-        json += "'" +  preguntas.item(i).textContent + "' :[\n" 
-            + "   '" + opciones.item(i).textContent.slice(26) + " '\n"
-            + "],\n";
+        json += "'" +  preguntas.item(i).textContent + "' :[\n" + addAnswer(i) + "],\n";
+    }
 
+    if (confirm('Compartir en Telegram')) {
         setTimeout(sendToTelegram(json), 5000);
-
-        // for(let j = 0; j < opciones_test.length; j += 2){
-        //     if(opciones_test.item(j).getAttribute('class').includes("correct")){
-        //         json += "   '" + opciones_test.item(j).textContent.slice(3) + "'\n";
-        //     }
-        // }
     }
 
     sessionStorage.setItem('jsonQuiz',json);    
 })
+
+function addAnswer(i){
+    if(revision == 0){
+        incorrect = 0;
+        opciones_test = opciones.item(i).childNodes;
+        for(let j = 0; j < opciones_test.length; j += 2){
+            if(opciones_test.item(j).getAttribute('class').includes(" correct")){
+                return "   '" + opciones_test.item(j).textContent.slice(3) + "'\n";
+            }
+            else incorrect += 2;
+        }
+
+        if(incorrect == opciones_test.length){
+            alert("No se ha encontrado la respuesta correcta a la pregunta " + (i+1));
+        }
+    }
+    else if(revision == 1){
+        return "   '" + opciones.item(i).textContent.slice(26) + " '\n"
+    }
+    else{
+        alert("Ha ocurrido un error");
+    }
+}
 
 function sendToTelegram(pregunta){    
     xhr.open("GET", urlString + `&text=${pregunta}`);
--- mantia.js
+++ mantia.js
@@ -1,11 +1,11 @@
 preguntas = document.getElementsByClassName("qtext");
 opciones = document.getElementsByClassName("answer");
-color = "4E4C4C";
+color = "0000FF";
 
 bbdd = {
     '¿En qué áreas se organiza el INCUAL?:' :[
         'Observatorio Profesional; investigación, metodología y articulación del SNCFP; diseño de las Cualificaciones e información y gestión de recursos. '],
-    'Las empresas que tienen una plantilla de 1 a 5 trabajadores NO tienen obligación de realizar aportación privada en formación bonificada.' :[
+    'Las empresas que tienen una plantilla de 1 a 5 trabajadores NO tienen obligación de realizar aportación privada en formación bonificada.' :[
         'Verdadero '],
     'Si supero los estudios de Formación Profesional Básica que titulación obtendré:' :[
         'Título Profesional Básico. '],
@@ -17,7 +17,30 @@ bbdd = {
         'Falso '],
     'Si tengo 15 años  ¿a cuál de los siguientes niveles de Formación Profesional puedo acceder?' :[
         'Formación Profesional Básica. '],
-    'Es el instrumento técnico, dotado de capacidad e independencia de criterios, que apoya al Consejo General de Formación Profesional para alcanzar los objetivos del Sistema Nacional de Cualificaciones y Formación Profesional:' :['Instituto Nacional de las Cualificaciones. '],'Los Certificados de profesionalidad de iniciativa privada están pendientes de desarrollo normativo:' :['Falso '],'Si quiero realizar una consulta de las cualificaciones profesionales existentes a cuál de los siguientes instrumentos consultaría:' :['Catálogo Nacional de las Cualificaciones Profesionales. '],'La formación profesional básica NO sustituye a los Programas de Cualificación Profesional Inicial:' :['Falso '],'Una cualificación es:' :['El conjunto de competencias profesionales con significación para el empleo que pueden ser adquiridas mediante formación modular u otros tipos de formación e incluso por la experiencia laboral. '],'La formación profesional inicial está dentro de la estructura del sistema educativo de nuestro país, pero tiene como referencia el Catalogo Nacional de las Cualificaciones profesionales para la elaboración de sus títulos.' :['Verdadero. '],'¿De qué iniciativa forman parte los programas públicos de empleo y formación?' :['Formación en alternancia con el empleo. '],'La formación  en alternancia con el empleo es una de las iniciativas de:' :['Formación profesional para el empleo. '],'La formación de oferta está dirigida prioritariamente a desempleados:' :['Falso '],'La siguiente definición: “Está integrado por el conjunto de instrumentos y acciones que tienen por objeto impulsar y extender entre las empresas y los trabajadores ocupados y desempleados una formación que responda a sus necesidades y contribuya al desarrollo de una economía basada en el conocimiento” a qué término corresponde:' :['Ninguna es correcta. '],'El  título de Técnico le obtengo si supero los estudios de:' :['Formación Profesional de Grado Medio. '],'¿Cuál de las siguientes situaciones me facilita acceder al Formación Profesional de Grado Superior?' :['Estar en posesión del Título de Bachiller, o de uncertificado acreditativo de haber superado todas las materias del Bachillerato. '],'La siguiente definición: “Es un conjunto de instrumentos y acciones necesarios para promover y desarrollar la integración de las ofertas de la formación profesional” a qué termino corresponde:' :[
+    'Es el instrumento técnico, dotado de capacidad e independencia de criterios, que apoya al Consejo General de Formación Profesional para alcanzar los objetivos del Sistema Nacional de Cualificaciones y Formación Profesional:' :[
+        'Instituto Nacional de las Cualificaciones. '],
+    'Los Certificados de profesionalidad de iniciativa privada están pendientes de desarrollo normativo:' :[
+        'Falso '],
+    'Si quiero realizar una consulta de las cualificaciones profesionales existentes a cuál de los siguientes instrumentos consultaría:' :[
+    'Catálogo Nacional de las Cualificaciones Profesionales. '],'La formación profesional básica NO sustituye a los Programas de Cualificación Profesional Inicial:' :[
+        'Falso '],
+    'Una cualificación es:' :[
+        'El conjunto de competencias profesionales con significación para el empleo que pueden ser adquiridas mediante formación modular u otros tipos de formación e incluso por la experiencia laboral. '],
+    'La formación profesional inicial está dentro de la estructura del sistema educativo de nuestro país, pero tiene como referencia el Catalogo Nacional de las Cualificaciones profesionales para la elaboración de sus títulos.' :[
+        'Verdadero. '],
+    '¿De qué iniciativa forman parte los programas públicos de empleo y formación?' :[
+        'Formación en alternancia con el empleo. '],
+    'La formación en alternancia con el empleo es una de las iniciativas de:' :[
+        'Formación profesional para el empleo. '],
+    'La formación de oferta está dirigida prioritariamente a desempleados:' :[
+        'Falso '],
+    'La siguiente definición: “Está integrado por el conjunto de instrumentos y acciones que tienen por objeto impulsar y extender entre las empresas y los trabajadores ocupados y desempleados una formación que responda a sus necesidades y contribuya al desarrollo de una economía basada en el conocimiento” a qué término corresponde:' :[
+        'Ninguna es correcta. '],
+    'El  título de Técnico le obtengo si supero los estudios de:' :[
+        'Formación Profesional de Grado Medio. '],
+    '¿Cuál de las siguientes situaciones me facilita acceder al Formación Profesional de Grado Superior?' :[
+        'Estar en posesión del Título de Bachiller, o de uncertificado acreditativo de haber superado todas las materias del Bachillerato. '],
+    'La siguiente definición: “Es un conjunto de instrumentos y acciones necesarios para promover y desarrollar la integración de las ofertas de la formación profesional” a qué termino corresponde:' :[
         'Sistema Nacional de las Cualificaciones y de la Formación Profesional. '],
 }
 
@@ -37,15 +60,13 @@ setTimeout(() => {
                     respuesta = opciones_test.item(k).textContent.slice(3);
 
                     if (value[0].includes(respuesta)) {
-                        pregunta[j].onclick = function(event) {
+                        document.getElementsByClassName("qtext").item(j).onclick = function(event) {
                             if (event === undefined) event = window.event;
-                            // var target = 'target' in event? event.target : event.srcElement;
-                        
-                            //Click
-                            opciones_test.item(k).childNodes[1].click();
+                            document.getElementsByClassName("answer").item(j).childNodes[k].childNodes[0].click();
                         };
-
+                        
                         opciones_test.item(k).childNodes[1].setAttribute("style","color:#" + color);
+                        
                     }
                 }
             }
