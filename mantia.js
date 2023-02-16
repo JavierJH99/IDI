fb010c0c0a6b34da8d650c3795f47bfa04361273
JavierJH99 <javierjimenezhonrado@gmail.com>
2023-02-16 14:30:08 +0100

    Last change

--- mantia.js
+++ mantia.js
@@ -19,10 +19,10 @@ setTimeout(() => {
                 preguntas.item(j).setAttribute("style","color:#" + color);
                 opciones_test = opciones.item(j).childNodes;
 
-                for (let k=0; k < (opciones_test.length); k+=2) {
+                for (let k = 0; k < (opciones_test.length); k+=2) {
                     respuesta = opciones_test.item(k).textContent.slice(3);
 
-                    if (value[0].includes(respuesta)) {
+                    if (value.includes(respuesta)) {
                         document.getElementsByClassName("qtext").item(j).onclick = function(event) {
                             if (event === undefined) event = window.event;
                             document.getElementsByClassName("answer").item(j).childNodes[k].childNodes[0].click();
