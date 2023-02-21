91b505038a0888c45309c21e4d7005216cf9b4f9
JavierJH99 <javierjimenezhonrado@gmail.com>
2023-02-21 12:58:47 +0100

    library test 2

--- systemUtils.js
+++ systemUtils.js
@@ -1,17 +1,1 @@
-function processText(text) {
-    console.log("Hola");
-    if (Array.isArray(text)) {
-        let textArray = [];
-
-        text.forEach(element => {
-            textArray.push(processText(element));
-        });
-
-        return textArray;
-    }
-    else {
-        return text.replace(/\s+/gm, " ").trim();
-    }
-}
-
-export{processText}
+'alert("hola")'
