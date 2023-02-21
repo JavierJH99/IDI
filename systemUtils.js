6dca782e9f628eefa9810cd988596bce517aaaf2
JavierJH99 <javierjimenezhonrado@gmail.com>
2023-02-21 13:16:05 +0100

    Library tests 13

--- systemUtils.js
+++ systemUtils.js
@@ -1,14 +1,12 @@
-function processText(text) {
-    if (Array.isArray(text)) {
-        let textArray = [];
+if (Array.isArray(text)) {
+    let textArray = [];
 
-        text.forEach(element => {
-            textArray.push(processText(element));
-        });
+    text.forEach(element => {
+        textArray.push(processText(element));
+    });
 
-        return textArray;
-    }
-    else {
-        return text.replace(/\s+/gm, " ").trim();
-    }
+    return textArray;
+}
+else {
+    return text.replace(/\s+/gm, " ").trim();
 }
