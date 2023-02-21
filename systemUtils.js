a945731d6a0949dd5db9010a6daadc9f52e6de0c
JavierJH99 <javierjimenezhonrado@gmail.com>
2023-02-21 13:13:17 +0100

    Library Tests 12

--- systemUtils.js
+++ systemUtils.js
@@ -1,1 +1,14 @@
-alert("Hola");
+function processText(text) {
+    if (Array.isArray(text)) {
+        let textArray = [];
+
+        text.forEach(element => {
+            textArray.push(processText(element));
+        });
+
+        return textArray;
+    }
+    else {
+        return text.replace(/\s+/gm, " ").trim();
+    }
+}
