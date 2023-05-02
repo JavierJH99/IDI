a8d30cdd20e25318c834444d57e24b157cc0497f
JavierJH99 <javierjimenezhonrado@gmail.com>
2023-05-02 13:32:20 +0200

    Fixed library errors

--- restartTest.js
+++ restartTest.js
@@ -1,15 +1,29 @@
+var xhr = new XMLHttpRequest();
+var urlLibrary = "https://raw.githubusercontent.com/JavierJH99/IDI/master/Libraries/systemUtils.json";
+
 var listAnswer = document.getElementsByClassName("answer");
 const removeElementsByClass = getFunctionLibrary("removeElementsByClass()");
 
-for (let i = 0; i < listAnswer.length; i++) {
-    for (let j = 0; j < listAnswer.item(i).childNodes.length; j++) {
-        if(listAnswer.item(i).childNodes.item(j).firstChild != null){
-            //.removeAttribute("disabled")
-            listAnswer.item(i).childNodes.item(j).firstChild.removeAttribute("disabled");
-            // .removeAttribute("checked")
-            listAnswer.item(i).childNodes.item(j).firstChild.removeAttribute("checked");
+setTimeout(() => {
+    for (let i = 0; i < listAnswer.length; i++) {
+        for (let j = 0; j < listAnswer.item(i).childNodes.length; j++) {
+            if(listAnswer.item(i).childNodes.item(j).firstChild != null){
+                //.removeAttribute("disabled")
+                listAnswer.item(i).childNodes.item(j).firstChild.removeAttribute("disabled");
+                // .removeAttribute("checked")
+                listAnswer.item(i).childNodes.item(j).firstChild.removeAttribute("checked");
+            }
         }
     }
-}
 
-removeElementsByClass("icon fa fa-check text-success fa-fw ");
+    removeElementsByClass("icon fa fa-check text-success fa-fw ");
+})
+
+function getFunctionLibrary(functionName) {
+    xhr.open("GET", urlLibrary, false);
+    xhr.send();
+    var textFunction = JSON.parse(xhr.responseText);
+    var parsedFunction = new Function(textFunction[functionName].parametros, textFunction[functionName].definicion)
+
+    return parsedFunction;
+}
